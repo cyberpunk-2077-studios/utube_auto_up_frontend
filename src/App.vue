da5227e778324c1bccdfc431c15549f0d90c8051
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2023-01-25 16:19:59 -0300

    Cropped footer from index

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <Header />
     <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
 import Main from '@/components/Main.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
     Header,
-    Main
+    Main,
+    Footer
   }
 }
 </script>
