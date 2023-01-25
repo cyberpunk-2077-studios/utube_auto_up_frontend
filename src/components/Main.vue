40e8e6e3e40b5ab51250313eeae24dc561379f08
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2023-01-25 16:34:36 -0300

    cropped album item video

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -21,46 +21,14 @@
 
         <template v-for="i in items" :key="i">
           <AlbumItem />
-        </template>      
+        </template>   
+
+        <template v-for="i in items" :key="i">
+          <AlbumItemVideo />
+        </template>     
       
 
-        <div class="col">
-          <div class="card shadow-sm">
-            <video
-            id="my-video"
-            class="video-js"
-            autoplay
-            controls
-            loop
-            preload="auto"
-            width="299"
-            height="225"            
-            data-setup="{}"
-          >
-            <source src="MY_VIDEO.mp4" type="video/mp4" />
-            <source src="MY_VIDEO.webm" type="video/webm" />
-            <p class="vjs-no-js">
-              To view this video please enable JavaScript, and consider upgrading to a
-              web browser that
-              <a href="https://videojs.com/html5-video-support/" target="_blank"
-                >supports HTML5 video</a
-              >
-            </p>
-          </video>
-            <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
+       
         
        
       </div>
@@ -72,8 +40,9 @@
 
 <script>
 import AlbumItem from '@/components/AlbumItem.vue'
+import AlbumItemVideo from '@/components/AlbumItemVideo.vue'
 export default {
-  components: { AlbumItem },
+  components: { AlbumItem, AlbumItemVideo },
   name: "MyMain",
   data(){
     return {
